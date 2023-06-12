4f317af6a43474f4516f7dc43995e8f68a01f65b
Alexander Alexeev <alekseev@trueconf.ru>
2023-06-12 22:41:12 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+/* for Base entities */
+export interface IBaseEntity {
+  title: string;
+  visibility: boolean;
+}
+
+/* for Files */
+export interface IFile {
+  fileName: string;
+  fileSrc: string;
+}
+
+/* for Socials */
+export interface ISocial extends IBaseEntity {
+  url: string;
+}
+
+export type TSocialList = Array<ISocial>;
+
+/* for Skills */
+export interface ISkill extends IBaseEntity {
+  experience: string;
+  color: string;
+}
+
+export type TSkillList = Array<ISkill>;
+
+/* for Projects */
+export interface IProject extends IBaseEntity, IFile {
+  description: string;
+  mainTechnology: string;
+  releaseDate: Date;
+  repository: string;
+  demo: string;
+  technologies: Array<string>;
+}
+
+export type TProjectList = Array<IProject>;
+
+/* for Personal info */
+export interface IPersonalInfo {
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  aboutMe: string;
+  country: string;
+  town: string;
+}
